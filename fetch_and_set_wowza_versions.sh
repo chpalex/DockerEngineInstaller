dfb703f0f2f9189ad03bda3b5fc73bfe37f2eabe
alex-chepurnoy <59021023+alex-chepurnoy@users.noreply.github.com>
2025-01-04 01:29:29 -0500

    Update fetch_and_set_wowza_versions.sh

--- fetch_and_set_wowza_versions.sh
+++ fetch_and_set_wowza_versions.sh
@@ -3,37 +3,16 @@
 # Function to fetch and select Wowza Engine version
 fetch_and_set_wowza_versions() {
   # Fetch all available versions of Wowza Engine from Docker
-  all_versions=$(curl -s "https://registry.hub.docker.com/v2/repositories/wowzamedia/wowza-streaming-engine-linux/tags" | jq -r '.results[] | "\(.name) \(.last_updated)"')
-
-  # Sort versions by date released and remove the date field
-  sorted_versions=$(echo "$all_versions" | sort -k2 -r | awk '{print $1}')
-
-  # Convert sorted versions to a format suitable for whiptail
-  version_list=()
-  column1=()
-  column2=()
-  count=0
-  while IFS= read -r version; do
-    if (( count % 2 == 0 )); then
-      column1+=("$version")
-    else
-      column2+=("$version")
-    fi
-    count=$((count + 1))
-  done <<< "$sorted_versions"
-
-  # Combine columns into a single list for whiptail
-  combined_list=()
-  for ((i = 0; i < ${#column1[@]}; i++)); do
-    combined_list+=("${column1[i]}" "${column2[i]:-}")
+  all_versions=""
+  url="https://registry.hub.docker.com/v2/repositories/wowzamedia/wowza-streaming-engine-linux/tags"
+  while [ -n "$url" ]; do
+    response=$(curl -s "$url")
+    tags=$(echo "$response" | jq -r '.results[] | "\(.name) \(.last_updated)"')
+    all_versions="$all_versions"$'\n'"$tags"
+    url=$(echo "$response" | jq -r '.next')
   done
-
-  # Calculate the height of the menu based on the number of versions
-  menu_height=$((${#combined_list[@]} / 2 + 10))
-  [ $menu_height -gt 40 ] && menu_height=40  # Limit the height to 40
-
   # Use whiptail to create a menu for selecting the version
-  engine_version=$(whiptail --title "Select Wowza Engine Version" --menu "Choose a version:" $menu_height 78 ${#combined_list[@]} "${combined_list[@]}" 3>&1 1>&2 2>&3)
+  engine_version=$(whiptail --title "Select Wowza Engine Version" --menu $menu_height 78 ${#combined_list[@]} "${combined_list[@]}" 3>&1 1>&2 2>&3)
 
   # Check if the user selected a version
   if [ $? -eq 0 ]; then
