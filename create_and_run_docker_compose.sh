ec4a97c607a1f0b080858411670fe9bf8ea24ad9
alex-chepurnoy <59021023+alex-chepurnoy@users.noreply.github.com>
2025-01-04 14:11:53 -0500

    Update create_and_run_docker_compose.sh

--- create_and_run_docker_compose.sh
+++ create_and_run_docker_compose.sh
@@ -8,12 +8,26 @@ create_and_run_docker_compose() {
   local wse_mgr_user=$4
   local wse_mgr_pass=$5
 
+  # Prompt for Docker container name
+  container_name=$(whiptail --inputbox "Enter Docker container name (default: wse_${engine_version}):" 8 78 "wse_${engine_version}" --title "Docker Container Name" 3>&1 1>&2 2>&3)
+  if [ $? -ne 0 ] || [ -z "$container_name" ]; then
+    container_name="wse_${engine_version}"
+  fi
+
+  # Define volume directories
+  logs_dir="$build_dir/${container_name}_logs"
+  content_dir="$build_dir/${container_name}_content"
+
+  # Create volume directories
+  mkdir -p "$logs_dir"
+  mkdir -p "$content_dir"
+
   # Create docker-compose.yml
   cat <<EOL > docker-compose.yml
 services:
   wowza:
     image: docker.io/library/wowza_engine:${engine_version}
-    container_name: wse_${engine_version}
+    container_name: ${container_name}
     restart: always
     ports:
       - "6970-7000:6970-7000/udp"
@@ -22,8 +36,8 @@ services:
       - "554:554"
       - "8084-8090:8084-8090/tcp"
     volumes:
-      - $build_dir/DockerEngine_Logs:/usr/local/WowzaStreamingEngine/logs
-      - $build_dir/DockerEngine_content:/usr/local/WowzaStreamingEngine/content
+      - $logs_dir:/usr/local/WowzaStreamingEngine/logs
+      - $content_dir:/usr/local/WowzaStreamingEngine/content
 
     entrypoint: /sbin/entrypoint.sh
     env_file: 
