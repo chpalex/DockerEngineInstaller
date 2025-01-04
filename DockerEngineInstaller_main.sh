3959b9eece3df974f192b8a3a0009e9ffb4b5d2f
alex-chepurnoy <59021023+alex-chepurnoy@users.noreply.github.com>
2025-01-04 14:01:47 -0500

    Update DockerEngineInstaller_main.sh

--- DockerEngineInstaller_main.sh
+++ DockerEngineInstaller_main.sh
@@ -27,6 +27,7 @@ TUNING_SCRIPT_URL="https://raw.githubusercontent.com/alex-chepurnoy/DockerEngine
 CREATE_DOCKER_IMAGE_SCRIPT_URL="https://raw.githubusercontent.com/alex-chepurnoy/DockerEngineInstaller/refs/heads/main/create_docker_image.sh"
 PROMPT_CREDENTIALS_SCRIPT_URL="https://raw.githubusercontent.com/alex-chepurnoy/DockerEngineInstaller/refs/heads/main/prompt_credentials.sh"
 CREATE_AND_RUN_DOCKER_COMPOSE_SCRIPT_URL="https://raw.githubusercontent.com/alex-chepurnoy/DockerEngineInstaller/refs/heads/main/create_and_run_docker_compose.sh"
+ENGINE_FILE_FETCH_SCRIPT_URL="https://raw.githubusercontent.com/alex-chepurnoy/DockerEngineInstaller/refs/heads/main/engine_file_fetch.sh"
 
 # Download the Functions Scripts
 curl -o "$SCRIPT_DIR/install_dependencies.sh" "$DEPENDENCIES_SCRIPT_URL" > /dev/null 2>&1
@@ -36,6 +37,7 @@ curl -o "$SCRIPT_DIR/tuning.sh" "$TUNING_SCRIPT_URL" > /dev/null 2>&1
 curl -o "$SCRIPT_DIR/create_docker_image.sh" "$CREATE_DOCKER_IMAGE_SCRIPT_URL" > /dev/null 2>&1
 curl -o "$SCRIPT_DIR/prompt_credentials.sh" "$PROMPT_CREDENTIALS_SCRIPT_URL" > /dev/null 2>&1
 curl -o "$SCRIPT_DIR/create_and_run_docker_compose.sh" "$CREATE_AND_RUN_DOCKER_COMPOSE_SCRIPT_URL" > /dev/null 2>&1
+curl -o "$SCRIPT_DIR/engine_file_fetch.sh" "$ENGINE_FILE_FETCH_SCRIPT_URL" > /dev/null 2>&1
 
 # Source for the Functions Scripts
 source "$SCRIPT_DIR/install_dependencies.sh"
@@ -45,6 +47,7 @@ source "$SCRIPT_DIR/tuning.sh"
 source "$SCRIPT_DIR/create_docker_image.sh"
 source "$SCRIPT_DIR/prompt_credentials.sh"
 source "$SCRIPT_DIR/create_and_run_docker_compose.sh"
+source "$SCRIPT_DIR/engine_file_fetch.sh"
 
 # Check if Docker is installed
 echo "   -----Checking if Docker is installed-----"
@@ -62,20 +65,10 @@ fi
 # Fetch and set Wowza Engine version
 engine_version=$(fetch_and_set_wowza_versions)
 
-## Create the Server.xml and VHost.xml files
-echo "   -----Creating Server.xml and VHost.xml for SSL file-----"
+# Copy Engine files from the Wowza Engine Docker image
+engine_file_fetch "$engine_version" "$BASE_DIR"
 
-# Create a temporary container from the image
-sudo docker run -d --name temp_container --entrypoint /sbin/entrypoint.sh wowzamedia/wowza-streaming-engine-linux:${engine_version} > /dev/null
-
-# Copy the VHost.xml file from the container to the host
-sudo docker cp temp_container:/usr/local/WowzaStreamingEngine/conf/VHost.xml "$BASE_DIR/VHost.xml"
-sudo docker cp temp_container:/usr/local/WowzaStreamingEngine/conf/Server.xml "$BASE_DIR/Server.xml"
-
-# Remove the temporary container
-sudo docker rm -f temp_container > /dev/null
-
-# Handle JKS file detection and setup
+# Handle SSL Configuration
 check_for_jks
 
 # Tune the Wowza Streaming Engine configuration
@@ -116,16 +109,14 @@ fi
 public_ip=$(curl -s ifconfig.me)
 
 # Get the private IP address
-private_ip=$(ip addr show | grep 'inet ' | grep -v 127.0.0.1 | awk '{print $2}' | cut -d/ -f1 | head -n 1)
+private_ip=$(ip route get 1 | awk '{print $7;exit}')
 
 # Print instructions to stop WSE and connect to Wowza Streaming Engine Manager
 echo "
 To stop WSE, type: sudo docker compose -f $COMPOSE_DIR/docker-compose.yml down
-
 "
 echo "
 Check $BUILD_DIR for Engine Logs and contents directories
-
 "
 if [ -n "$jks_domain" ]; then
   echo "To connect to Wowza Streaming Engine Manager over SSL, go to: https://${jks_domain}:8090/enginemanager"
