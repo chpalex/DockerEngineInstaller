ef0e1864d24ffb567a4ff84a85efd6b64cb53a46
alex-chepurnoy <59021023+alex-chepurnoy@users.noreply.github.com>
2025-01-04 01:14:01 -0500

    Update fetch_and_set_wowza_versions.sh

--- fetch_and_set_wowza_versions.sh
+++ fetch_and_set_wowza_versions.sh
@@ -3,37 +3,24 @@
 # Function to fetch and select Wowza Engine version
 fetch_and_set_wowza_versions() {
   # Fetch all available versions of Wowza Engine from Docker
-  all_versions=""
-  url="https://registry.hub.docker.com/v2/repositories/wowzamedia/wowza-streaming-engine-linux/tags"
-  while [ -n "$url" ]; do
-    response=$(curl -s "$url")
-    tags=$(echo "$response" | jq -r '.results[] | "\(.name) \(.last_updated)"')
-    all_versions="$all_versions"$'\n'"$tags"
-    url=$(echo "$response" | jq -r '.next')
-  done
+  all_versions=$(curl -s "https://registry.hub.docker.com/v2/repositories/wowzamedia/wowza-streaming-engine-linux/tags" | jq -r '.results[] | "\(.name) \(.last_updated)"')
 
-  # Sort versions by date released
-  sorted_versions=$(echo "$all_versions" | sort -k2 -r)
+  # Sort versions by date released and remove the date field
+  sorted_versions=$(echo "$all_versions" | sort -k2 -r | awk '{print $1}')
 
-  # Remove the date field and display only the version names
-  sorted_versions=$(echo "$sorted_versions" | awk '{print $1}')
+  # Display the sorted versions
   echo "All available versions sorted by date released:"
   echo "$sorted_versions"
 
   # Prompt user for version of the engine and verify if it exists
   while true; do
     read -p "Enter the version of the engine you want to build from the list above: " engine_version
-    echo "User entered: $engine_version"
-    echo "Checking if $engine_version exists in the sorted versions list..."
     if echo "$sorted_versions" | grep -q "^${engine_version}$"; then
-      echo "Selected version: $engine_version"
-      break
+      echo "$engine_version"
+      return
     else
       echo "Error: The specified version ${engine_version} does not exist. Please enter a valid version from the list below:"
       echo "$sorted_versions"
     fi
   done
-
-  # Return the selected version
-  echo "$engine_version"
 }
