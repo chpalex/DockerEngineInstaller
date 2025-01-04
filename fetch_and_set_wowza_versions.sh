6a7739667dcb4cce572509e11edb6fe8a9e86065
alex-chepurnoy <59021023+alex-chepurnoy@users.noreply.github.com>
2025-01-04 01:31:23 -0500

    Update fetch_and_set_wowza_versions.sh

--- fetch_and_set_wowza_versions.sh
+++ fetch_and_set_wowza_versions.sh
@@ -11,8 +11,22 @@ fetch_and_set_wowza_versions() {
     all_versions="$all_versions"$'\n'"$tags"
     url=$(echo "$response" | jq -r '.next')
   done
+  
+  # Sort versions by date released and remove the date field
+  sorted_versions=$(echo "$all_versions" | sort -k2 -r | awk '{print $1}')
+
+  # Convert sorted versions to a format suitable for whiptail
+  version_list=()
+  while IFS= read -r version; do
+    version_list+=("$version" "")
+  done <<< "$sorted_versions"
+
+  # Calculate the height of the menu based on the number of versions
+  menu_height=$((${#version_list[@]} / 2 + 10))
+  [ $menu_height -gt 40 ] && menu_height=40  # Limit the height to 40
+
   # Use whiptail to create a menu for selecting the version
-  engine_version=$(whiptail --title "Select Wowza Engine Version" --menu $menu_height 78 ${#combined_list[@]} "${combined_list[@]}" 3>&1 1>&2 2>&3)
+  engine_version=$(whiptail --title "Select Wowza Engine Version" --menu $menu_height 78 ${#version_list[@]} "${version_list[@]}" 3>&1 1>&2 2>&3)
 
   # Check if the user selected a version
   if [ $? -eq 0 ]; then
