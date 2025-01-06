3b2ff9964183d7c4beedc7a5cf68e6b92d0b27d3
alex-chepurnoy <59021023+alex-chepurnoy@users.noreply.github.com>
2025-01-06 08:24:09 -0500

    Update cleanup.sh
    added downloaded scripts cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -4,6 +4,7 @@
 cleanup() {
   local base_dir=$1
   local build_dir=$2
+  local SCRIPT_DIR=$3
 
   echo "Cleaning up the install directory..."
 
@@ -26,4 +27,39 @@ cleanup() {
   if [ -f "$base_dir/log4j2-config.xml" ]; then
     sudo rm "$base_dir/log4j2-config.xml"
   fi
+
+  #Remove downloaded script files
+  if [  -f "$SCRIPT_DIR/tuning.sh" ]; then
+  sudo rm "$SCRIPT_DIR/tuning.sh"
+  fi
+
+  if [  -f "$SCRIPT_DIR/install_dependencies.sh" ]; then 
+  sudo rm "$SCRIPT_DIR/install_dependencies.sh"
+  fi
+
+  if [  -f "$SCRIPT_DIR/fetch_and_set_wowza_versions.sh" ]; then
+  sudo rm "$SCRIPT_DIR/fetch_and_set_wowza_versions.sh"
+  fi
+
+  if [  -f "$SCRIPT_DIR/jks_functions.sh" ]; then
+  sudo rm "$SCRIPT_DIR/jks_functions.sh"
+  fi
+
+  if [  -f "$SCRIPT_DIR/create_docker_image.sh" ]; then
+  sudo rm "$SCRIPT_DIR/create_docker_image.sh"
+  fi
+
+  if [  -f "$SCRIPT_DIR/prompt_credentials.sh" ]; then
+  sudo rm "$SCRIPT_DIR/prompt_credentials.sh"
+  fi
+
+  if [  -f "$SCRIPT_DIR/create_and_run_docker_compose.sh" ]; then
+  sudo rm "$SCRIPT_DIR/create_and_run_docker_compose.sh"
+  fi
+
+  if [  -f "$SCRIPT_DIR/engine_file_fetch.sh" ]; then
+  sudo rm "$SCRIPT_DIR/engine_file_fetch.sh"
+  fi
+
+  rm -- "$0"
 }
