3570f985430c2520c240f66e046a5ec33e11150f
chpalex <alex.olya@gmail.com>
2025-01-10 14:11:06 -0500

    remove previous code

--- DockerEngineInstaller_master.sh
+++ DockerEngineInstaller_master.sh
@@ -267,57 +267,6 @@ FROM wowzamedia/wowza-streaming-engine-linux:${engine_version}
 
 RUN apt update && apt install -y nano
 WORKDIR /usr/local/WowzaStreamingEngine/
-RUN cat <<'EOF' > entrypoint.sh
-#!/bin/bash
-
-WMSAPP_HOME="$( readlink /usr/local/WowzaStreamingEngine )"
-
-if [ -z $WSE_MGR_USER ]; then
-	mgrUser="wowza"
-else
-	mgrUser=$WSE_MGR_USER
-fi
-if [ -z $WSE_MGR_PASS ]; then
-	mgrPass="wowza"
-else
-	mgrPass=$WSE_MGR_PASS
-fi
-
-if [ ! -z $WSE_LIC ]; then
-cat > ${WMSAPP_HOME}/conf/Server.license <<EOF
------BEGIN LICENSE-----
-${WSE_LIC}
------END LICENSE-----
-EOF
-fi
-
-# Check if admin.password file exists and add user if not
-if [ ! -f \"\${WMSAPP_HOME}/conf/admin.password\" ] || ! grep -q \"^\${mgrUser}\" \"\${WMSAPP_HOME}/conf/admin.password\"; then
-echo -e "\n$mgrUser $mgrPass admin|advUser\n" >> ${WMSAPP_HOME}/conf/admin.password
-echo -e "\n$mgrUser $mgrPass\n" >> ${WMSAPP_HOME}/conf/publish.password
-echo -e "\n$mgrUser $mgrPass\n" >> ${WMSAPP_HOME}/conf/jmxremote.password
-#echo -e "$mgrUser readwrite\n" >> ${WMSAPP_HOME}/conf/jmxremote.access
-fi
-
-if [[ ! -z $WSE_IP_PARAM ]]; then
-	#change localhost to some user defined IP
-	cat "${WMSAPP_HOME}/conf/Server.xml" > serverTmp
-	sed 's|\(<IpAddress>localhost</IpAddress>\)|<IpAddress>'"$WSE_IP_PARAM"'</IpAddress> <!--changed for default install. \1-->|' <serverTmp >Server.xml
-	sed 's|\(<RMIServerHostName>localhost</RMIServerHostName>\)|<RMIServerHostName>'"$WSE_IP_PARAM"'</RMIServerHostName> <!--changed for default install. \1-->|' <Server.xml >serverTmp
-	cat serverTmp > ${WMSAPP_HOME}/conf/Server.xml
-	rm serverTmp Server.xml
-	
-	cat "${WMSAPP_HOME}/conf/VHost.xml" > vhostTmp
-	sed 's|\(<IpAddress>${com.wowza.wms.HostPort.IpAddress}</IpAddress>\)|<IpAddress>'"$WSE_IP_PARAM"'</IpAddress> <!--changed for default cloud install. \1-->|' <vhostTmp >${WMSAPP_HOME}/conf/VHost.xml 
-	rm vhostTmp
-fi
-
-# Make supervisor log files configurable
-#sed 's|^logfile=.*|logfile='"${SUPERVISOR_LOG_HOME}"'/supervisor/supervisord.log ;|' -i /etc/supervisor/supervisord.conf
-
-exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
-EOF
-RUN chmod +x /sbin/entrypoint.sh
 
 # Create the tuning.sh script
 RUN cat <<'EOF' > tuning.sh
