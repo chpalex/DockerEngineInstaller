8f4a71bfe020706538f1c12343e9bd162a4a75ca
chpalex <alex.olya@gmail.com>
2025-01-20 16:30:00 -0500

    added noprompt

--- DockerEngineInstaller_master.sh
+++ DockerEngineInstaller_master.sh
@@ -719,7 +719,7 @@ convert_pem_to_jks() {
     # Convert PEM to PKCS12 and then to JKS inside the Docker container
     docker exec "$container_name" bash -c "
         openssl pkcs12 -export -in '$pem_dir/cert1.pem' -inkey '$pem_dir/privkey1.pem' -out '$jks_dir/$domain.p12' -name '$domain' -passout pass:$pkcs12_password &&
-        /usr/local/WowzaStreamingEngine/java/bin/keytool -importkeystore -deststorepass $jks_password -destkeypass $jks_password -destkeystore '$jks_dir/$domain.jks' -srckeystore '$jks_dir/$domain.p12' -srcstoretype PKCS12 -srcstorepass $pkcs12_password -alias '$domain' && 
+        /usr/local/WowzaStreamingEngine/java/bin/keytool -importkeystore -deststorepass $jks_password -destkeypass $jks_password -destkeystore '$jks_dir/$domain.jks' -srckeystore '$jks_dir/$domain.p12' -srcstoretype PKCS12 -srcstorepass $pkcs12_password -alias '$domain' -noprompt && 
         /usr/local/WowzaStreamingEngine/java/bin/keytool -import -trustcacerts -alias root -file '$pem_dir/chain1.pem' -keystore '$jks_dir/$domain.jks' -storepass $jks_password -noprompt &&
         /usr/local/WowzaStreamingEngine/java/bin/keytool -import -trustcacerts -alias chain -file '$pem_dir/fullchain1.pem' -keystore '$jks_dir/$domain.jks' -storepass $jks_password -noprompt
     "
