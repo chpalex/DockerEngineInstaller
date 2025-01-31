471cdeebe2a185700d844d1f6601704d2b9c4482
chpalex <165507912+chpalex@users.noreply.github.com>
2025-01-31 08:38:35 -0500

    welcome screen update

--- DockerEngineInstaller_master.sh
+++ DockerEngineInstaller_master.sh
@@ -16,7 +16,7 @@ root=,black'
 
 # Display info box about the script and function scripts
 whiptail --title "Docker Engine Workflow Installer" --msgbox "
-Welcome to the Docker Engine Workflow Installer!\n\nThis installation script automates the deployment of Wowza Streaming Engine, a simple webserver and SSL in a Docker environment." 20 75
+Welcome to the Docker Engine Workflow Installer!\n\nThis installation script automates the deployment of\n\n - Wowza Streaming Enginen\n\n - SWAG (simple webserver and SSL bot)\n\n - Portainer (docker management tool) in a Docker stack." 20 75
 
 #
 ## Set directory variables and create the directories
