bbc5fa1f613fed87811b539fa88cadfb1bd9a37d
chpalex <165507912+chpalex@users.noreply.github.com>
2025-01-31 08:09:42 -0500

    add more instructions for managing containers

--- DockerEngineInstaller_master.sh
+++ DockerEngineInstaller_master.sh
@@ -1155,12 +1155,14 @@ EOL
         <li>Copy files out: <code>sudo cp Engine_xxxx/[file_name] [file_name]</code></li>
         <li>Copy files in: <code>sudo cp [file_name] Engine_xxxx/[file_name]</code></li>
       </ul>
-      <p>NOTE: Container must be restarted for changes to take effect:</p>
+      <p>NOTE: Container must be restarted for changes to take effect: <code>cd $container_dir && sudo docker restart $container_name && cd $SCRIPT_DIR</code></p>
+      <p>To restart other containders, use swag or portainer in the same command</p>
       <p>To manage the state of the docker containers, use the following commands:</p>
       <ul>
-        <li>Stop and destroy the Docker Wowza container: <code>cd $container_dir && sudo docker compose down --rmi 'all' && cd $SCRIPT_DIR</code></li>
-        <li>Stop the container without destroying it: <code>cd $container_dir && sudo docker compose stop && cd $SCRIPT_DIR</code></li>
-        <li>Start the container after stopping it: <code>cd $container_dir && sudo docker compose start && cd $SCRIPT_DIR</code></li>
+        <li>Stop and destroy the Docker Wowza, swag and portainer container stack: <code>cd $container_dir && sudo docker compose down --rmi 'all' && cd $SCRIPT_DIR</code></li>
+        <li>Stop the container stack without destroying it: <code>cd $container_dir && sudo docker compose stop && cd $SCRIPT_DIR</code></li>
+        <li>Start the container stack after stopping it: <code>cd $container_dir && sudo docker compose start && cd $SCRIPT_DIR</code></li>
+        <li>Restart the container stack: <code>cd $container_dir && sudo docker compose restart && cd $SCRIPT_DIR</code></li>
       </ul>
       <p>To delete volumes, use the following command:</p>
       <ul>
@@ -1170,6 +1172,10 @@ EOL
       <p>To access the container directly, type:
       <ul>
         <li><code>sudo docker exec -it $container_name bash</code></li>
+      </ul>
+      <p>To nuke the whole thing and start over, use the following commands:</p>
+      <ul>
+        <li>sudo docker system prune -a --volumes -f</li>
       </ul>
     </div>
 
